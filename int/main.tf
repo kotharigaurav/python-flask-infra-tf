603676847c75f60ff7cba7026a6577a7984d7523
Gaurav Kothari <kotharigaurav3699@gmail.com>
2025-07-13 15:35:04 +0530

    adding terraform code and workflow file

--- int/main.tf
+++ int/main.tf
@@ -3,7 +3,7 @@
 ###################################
 
 module "iam_role_module" {
-  source    = "../../modules/iam"
+  source    = "../modules/iam"
   role_name = local.iam_role_name
   policy_name = local.iam_policy_name
 }
