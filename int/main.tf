bc45ca5d0b86bb3d99f2dbf57e8807c0dde9ae18
Gaurav Kothari <kotharigaurav3699@gmail.com>
2025-07-14 23:22:41 +0530

    testing code

--- int/main.tf
+++ int/main.tf
@@ -2,8 +2,16 @@
 # IAM Role
 ###################################
 
-module "iam_role_module" {
-  source    = "../modules/iam"
-  role_name = local.iam_role_name
-  policy_name = local.iam_policy_name
+# module "iam_role_module" {
+#   source    = "../modules/iam"
+#   role_name = local.iam_role_name
+#   policy_name = local.iam_policy_name
+# }
+
+output "aws_vpc_id" {
+  value = data.aws_vpc.selected.id
+}
+
+output "aws_vpc_arn" {
+  value = data.aws_vpc.selected.arn
 }
