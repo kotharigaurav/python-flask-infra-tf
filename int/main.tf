647a2e05ad474ee5ec38bb553e03ec19fca0caee
Gaurav Kothari <kotharigaurav3699@gmail.com>
2025-07-15 18:29:35 +0530

    adding module for subnet creation

--- int/main.tf
+++ int/main.tf
@@ -5,8 +5,8 @@
 module "vpc" {
   source   = "../modules/vpc"
   vpc_name = local.vpc_name
-  cidr_block = var.cidr_block
-  public_subnet_cidr = var.public_subnet_cidr
-  private_subnet_cidr = var.private_subnet_cidr
-  availability_zone = var.availability_zone
+  cidr_block = local.cidr_block
+  public_subnet_cidr = local.public_subnet_cidr
+  private_subnet_cidr = local.private_subnet_cidr
+  availability_zone = local.availability_zone
 }
