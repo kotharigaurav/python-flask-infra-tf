c182b7befd7050675ef5e9ed2640373f8fe3c051
Gaurav Kothari <kotharigaurav3699@gmail.com>
2025-07-16 22:13:07 +0530

    adding module for rt, ig creation

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -55,7 +55,7 @@ resource "aws_route_table" "public-rt" {
 }
 
 # Associate public subnets with the route table
-resource "aws_route_table_association" "rta" {
+resource "aws_route_table_association" "public-rta" {
   count = length(aws_subnet.public)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public-rt.id
@@ -71,7 +71,7 @@ resource "aws_route_table" "private-rt" {
 }
 
 # Associate private subnets with the route table
-resource "aws_route_table_association" "rta" {
+resource "aws_route_table_association" "private-rta" {
   count = length(aws_subnet.private)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private-rt.id
