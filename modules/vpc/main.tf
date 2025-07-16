e16591c411c2d03419c010768b21e394ad04c1d3
Gaurav Kothari <kotharigaurav3699@gmail.com>
2025-07-16 22:10:27 +0530

    adding module for rt, ig creation

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "public" {
   availability_zone = element(var.availability_zone, count.index  )
 
   tags = {
-    Name = "aws-public-subnet-${count.index + 1}"
+    Name = "aws-vpc-public-subnet-${count.index + 1}"
   }
 }
 
@@ -28,6 +28,51 @@ resource "aws_subnet" "private" {
   availability_zone = element(var.availability_zone, count.index)
 
   tags = {
-    Name = "aws private-subnet-${count.index + 1}"
+    Name = "aws-vpc-private-subnet-${count.index + 1}"
   }
+}
+
+# Create internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "aws-vpc-internet-gateway"
+  }
+}
+
+# Create route table for public subnets
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "aws-vpc-public-route-table"
+  }
+}
+
+# Associate public subnets with the route table
+resource "aws_route_table_association" "rta" {
+  count = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+# Create route table for private subnets
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "aws-vpc-private-route-table"
+  }
+}
+
+# Associate private subnets with the route table
+resource "aws_route_table_association" "rta" {
+  count = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private-rt.id
 }
