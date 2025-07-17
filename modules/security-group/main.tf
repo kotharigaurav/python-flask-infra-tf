50679315fb97c79df86c955b7609d3d0ba13e1b8
Gaurav Kothari <kotharigaurav3699@gmail.com>
2025-07-17 22:20:32 +0530

    adding sg module code

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "ec2_sg" {
 
   ingress {
     description = "Allow HTTP requests from anywhere"
-    cidr_blocks = ["0.0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
